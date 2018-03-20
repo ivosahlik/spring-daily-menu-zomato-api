2f4107d272c022089a7b02ab455da634c941b88f
Ivo Vošahlík <ivosahlik@seznam.cz>
2018-03-20 21:55:51 +0100

    initial commit

---
+++ src/main/java/cz/ivosahlik/zomatoapi/ZomatoApiApplication.java
@@ -0,0 +1,12 @@
+package cz.ivosahlik.zomatoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoApiApplication.class, args);
+	}
+}
